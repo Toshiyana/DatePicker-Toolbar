d2367d041355ee64d7721a3686cd1a3daa874f64
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-18 15:10:15 +0900

    Create DatePicker with toolbar

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
 
+    func createDatePicker() {
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // bar button
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneButtonPressed))
+        toolbar.setItems([doneButton], animated: true)
+        
+        // assign toolbar
+        dateTextField.inputAccessoryView = toolbar
+        
+        // assign datePicker to the textField
+        dateTextField.inputView = datePicker
+        
+        // dateTextField style
+        dateTextField.textAlignment = .center
+        
+        // datePicker style
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+    }
 
+    @objc func doneButtonPressed() {
+        // formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        dateTextField.text = formatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
 }
 
